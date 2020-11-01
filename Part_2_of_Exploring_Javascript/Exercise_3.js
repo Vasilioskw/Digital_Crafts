bb22ae02273b57a993bc54223cae24ee59e51def
Vasilios Kambouras <Vasilioskw@gmail.com>
2020-11-01 17:03:42 -0500

    moving stuff

---
+++ Part_2_of_Exploring_Javascript/Exercise_3.js
@@ -0,0 +1,41 @@
+// 1
+// Write a loop that will print the elements in the following array:
+// problemArray = [[‘Lion’,’Deer’,’Sheep’], [‘Cub’,’Fawn’,’Lamb’]]
+// The example output will look like:
+// [‘Lion’, ‘Deer’, ‘Sheep’]
+// [‘Cub’, ‘Fawn’, ‘Lamb’] 
+
+// function animals() {
+
+// bigAnimal = ["Lion" , "Deer", "Sheep"]
+
+// smallAnimal = ["Cub" , "Fawn", "Lamb"]
+
+// bigAnimal = [] console.log(bigAnimal[1],smallAnimal[1])
+
+
+// bigAnimal.shift();
+// smallAnimal.shift();
+
+// }
+// animals()
+
+// Correct Answer:
+
+problemArray = [["Lion" , "Deer", "Sheep"], ["Cub" , "Fawn", "Lamb"]]
+let i = 0; i < problemArray; i++;
+
+console.log(problemArray[i])
+
+let j = 0;  < problem.lenth; i++ {
+for (let j = 0; j < problemArray[i].length; j++)
+
+}
+
+// break 
+
+  
+  // 2
+  // Using the same array as in problem one, 
+  // use a nested for loop to access each individual 
+  // element from the array. Print each element to the console.
